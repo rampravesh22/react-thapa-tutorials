839f90e30059126a71de64841d80e32fae4a0cff
rpc <rpc7863@gamil.com>
2022-09-22 21:22:56 +0530

    login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import LoginForm from './LoginForm';
+import React from "react";
+import LoginForm from "./LoginForm";
 function App() {
 	return (
 		<>
 			{/* Componenents */}
-            <LoginForm></LoginForm>
-			
+			<LoginForm></LoginForm>
 		</>
 	);
 }
--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const LoginForm = ()=>{
-    return(
-        <>
-            <h3>Hello</h3>
-        </>
-    )
-}
+const LoginForm = () => {
+	const [fullName, setFullName] = useState();
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+
+	const submitForm = (e) => {
+		setFullName(`${firstName} ${lastName}`);
+
+		e.preventDefault();
+	};
+	const inputEvent1 = (e) => {
+		setFirstName(e.target.value);
+	};
+
+	const inputEvent2 = (e) => {
+		setLastName(e.target.value);
+	};
+	return (
+		<>
+			<div className="container">
+				<h1>Hello {fullName}</h1>
+				<form action="/" method="post" onSubmit={submitForm}>
+					<div className="form-input">
+						<input
+							type="text"
+							name=""
+							id=""
+							placeholder="Enter your name"
+							onChange={inputEvent1}
+							value={firstName}
+						/>
+					</div>
+					<div className="form-input">
+						<input
+							type="text"
+							name=""
+							id=""
+							placeholder="Enter last name"
+							onChange={inputEvent2}
+							value={lastName}
+						/>
+					</div>
+					<input type="submit" value="Submit" />
+				</form>
+			</div>
+		</>
+	);
+};
 export default LoginForm;

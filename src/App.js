d0d62332ab745b4660fe83bcc4319c52467b84d1
rpc <rpc7863@gamil.com>
2022-09-22 07:40:57 +0530

    djj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-const btn = document.querySelector("button");
-console.log(btn);
+import React,{useState} from 'react';
+
 function App() {
+	let [current, updateCurrent] = useState(0);
+	const IncNum = () => {
+		updateCurrent(current+1);
+	};
 	return (
 		<>
-			<h1>Welcome</h1>
-			
+			<h1 className="heading">{current}</h1>
+			<button onClick={IncNum}>Click Me</button>
 		</>
 	);
 }

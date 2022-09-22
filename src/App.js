eabeb301b9cab5362e0ee42bfb09f1322332ea61
rpc <rpc7863@gamil.com>
2022-09-22 13:51:04 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import LoginForm from './LoginForm';
 function App() {
 	return (
 		<>
 			{/* Componenents */}
+            <LoginForm></LoginForm>
 			
 		</>
 	);
---
+++ src/LoginForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LoginForm = ()=>{
+    return(
+        <>
+            <h3>Hello</h3>
+        </>
+    )
+}
+export default LoginForm;

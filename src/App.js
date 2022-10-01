aa1f4fea937e33e74a65ca82d92597b226eb411d
rpc <rpc7863@gamil.com>
2022-10-01 19:32:51 +0530

    router with nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import Name from "./Name";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
 	return (
 		<>
-			{/* Componenents */}
-			
+			<NavBar></NavBar>
+			<Routes>
+				<Route path="/"  element={<Home></Home>}></Route>
+				<Route path="/about" element={<About></About>}></Route>
+				<Route path="/contact" element={<Contact></Contact>}></Route>
+				<Route path="/contact/:name" element={<Name></Name>}></Route>
+				<Route path="*" element={<h1>Error</h1>}></Route>
+			</Routes>
 		</>
 	);
 }
---
+++ src/Contact.jsx
@@ -0,0 +1,38 @@
+const Contact = () => {
+	return (
+		<>
+			<div className="contact">
+            <h1>Contact page</h1>
+            <hr />
+            <p>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+               quis quia quasi provident architecto nesciunt aut quam cum
+               corporis. Possimus voluptatibus non ipsum ipsam consectetur
+               harum minima quam tempore illo. Sequi, nemo architecto debitis
+               fugiat libero pariatur vitae, iusto voluptates, omnis cum minus
+               voluptas odio unde quasi nesciunt dolorem? Ipsa reprehenderit,
+               veritatis qui ea vero, voluptatum porro, debitis eius earum
+               similique itaque. Tenetur atque sapiente quasi maiores veritatis
+               libero harum id animi molestias repellendus. Quo aliquid minima
+               voluptas atque! Nesciunt distinctio tenetur vel unde quos, iusto
+               aliquam amet, voluptas rerum harum quo cum possimus dolore
+               laudantium ratione delectus ipsa itaque reiciendis ex saepe
+               aspernatur quidem maxime. Esse, neque. Id deserunt explicabo cum
+               harum nisi? Aliquid repellat pariatur ducimus unde, officiis
+               earum deleniti enim aperiam placeat. Alias possimus ab dolore
+               dolorem ducimus ipsa nihil non veritatis consequatur
+               repudiandae? Accusantium voluptate, suscipit veniam ex repellat
+               maxime quas magni maiores, earum commodi dolorem excepturi.
+               Similique laboriosam porro at adipisci quae corporis facere
+               asperiores non dicta quisquam eligendi consequatur id explicabo
+               quam esse fuga corrupti, nisi molestiae atque deleniti eum sequi
+               ullam suscipit? Nobis impedit vel possimus libero. Tenetur quam
+               voluptates exercitationem consequuntur corrupti veritatis earum,
+               itaque dignissimos culpa sapiente neque, laboriosam saepe ut!
+            </p>
+         </div>
+		</>
+	);
+};
+
+export default Contact;

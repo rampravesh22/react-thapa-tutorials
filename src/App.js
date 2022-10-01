7971bcca01d8edc023247d7f8d9523494c8d2562
rpc <rpc7863@gamil.com>
2022-10-01 06:39:28 +0530

    use effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useEffect } from "react";
 function App() {
+	const [num, setSum] = useState(0);
+   useEffect(()=>{
+      alert("ckkkk")
+   },[num])
 	return (
 		<>
 			{/* Componenents */}
-			
+			<h1>{num}</h1>
+			<button
+				onClick={() => {
+					setSum(num + 1);
+				}}
+			>
+				+
+			</button>
 		</>
 	);
 }

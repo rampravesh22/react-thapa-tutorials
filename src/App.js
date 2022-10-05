c4717c893d123ac071ee3d4a9cdba91155823ea8
rpc <rpc7863@gamil.com>
2022-10-05 14:48:02 +0530

    bootstrap example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function App() {
 	return (
 		<>
 			{/* Componenents */}
-			
+			<h1 className="text-center bg bg-dark text-white py-3">
+				Hello there how are you
+			</h1>
+			<div className="text-center">
+				<button
+					className="btn btn-outline-danger fs-5"
+					style={{ fontSize: "100px",backgroundColor:"red" }}
+				>
+					Click
+				</button>
+			</div>
 		</>
 	);
 }

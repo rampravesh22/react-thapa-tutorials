6da0fb6b8aa45c0ca6a1c103780775e3fa1ec14c
rpc <rpc7863@gamil.com>
2022-10-05 16:58:44 +0530

    first commit in animated_website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Service from "./components/Service";
 function App() {
 	return (
 		<>
-			{/* Componenents */}
-			
+			<Routes>
+				<Route path="/" element={<Home></Home>}></Route>
+				<Route path="/about" element={<About></About>}></Route>
+				<Route path="/service" element={<Service></Service>}></Route>
+				<Route path="/contact" element={<Contact></Contact>}></Route>
+			</Routes>
 		</>
 	);
 }

329febbd7782c7b3386e67e28f3437cfc611ba89
rpc <rpc7863@gamil.com>
2022-10-09 14:35:48 +0530

    react with django

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function App() {
+	const [students, setStudents] = useState([]);
+	useEffect(() => {
+		const getAllStudent = async () => {
+			try {
+				const students = await axios.get(
+					"http://127.0.0.1:8000/student/"
+				);
+				// console.log(students.data);
+				setStudents(students.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getAllStudent();
+	});
 	return (
 		<>
-			{/* Componenents */}
-			
+			<h1>Connect django with react </h1>
+
+			<div className="container">{students.map((student) => {
+				return(
+					<>
+						<h1>{student.name}</h1>
+					</>
+				)
+			})}</div>
 		</>
 	);
 }
